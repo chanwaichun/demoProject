af178f461fd9bd81b300893d859823958c1360e9
chenwj <chenwj@cemyun.com>
2021-07-05 22:20:15 +0800

    新增context上下文

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2020-10-29 15:00:14
- * @LastEditTime: 2021-03-02 21:35:00
+ * @LastEditTime: 2021-06-15 23:00:22
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \demoProject\src\App.js
  */
-import React, { Component } from "react";
+import React, { Component, createContext } from "react";
 import MainRoute from "./page/MainRoute";
 import Login from "./page/Login";
+import NoMatch from "./page/NoMatch/index";
 import store from "./store";
 import {
   BrowserRouter as Router,
@@ -17,6 +18,7 @@ import {
   Switch,
 } from "react-router-dom";
 import "antd/dist/antd.less";
+const appStore = createContext();
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -26,13 +28,18 @@ export default class App extends Component {
 
   render() {
     return (
-      <Router basename="/">
-         {/* {!this.state.isLogin ? <Redirect to="/login" /> : null} */}
-      
-        <Route path="/" component={MainRoute} />
-        {/* <Route  path="/login" component={Login} />   */}
-       
-      </Router>
+      <appStore.Provider value={{ publicParams: "公共参数" }}>
+        <Router basename="/">
+          {/* {!this.state.isLogin ? <Redirect to="/login" /> : null} */}
+          <Switch>
+            
+            <Route path="/main" component={MainRoute} />
+            <Route exact path="/login" component={Login} />
+            {/* <Redirect to="/main"></Redirect> */}
+            {/* <Route component={NoMatch}></Route> */}
+          </Switch>
+        </Router>
+      </appStore.Provider>
     );
   }
 }
