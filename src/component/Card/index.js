9ce14663f6011b618e887662f6c117e9d6f429cc
chanwaichun <chanwaichuncc@163.com>
2021-07-09 00:00:19 +0800

    新增card'组件

--- src/component/Card/index.js
+++ src/component/Card/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-04 01:41:13
- * @LastEditTime: 2021-07-04 01:48:52
+ * @LastEditTime: 2021-07-06 00:11:37
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \demoProject\src\component\Card\index.js
@@ -12,10 +12,14 @@ import { Card as AntdCard } from "antd";
 const DEFAULT_CONFIG = {
   title: "Card title",
   bordered: false,
-  style: { width: 300 },
 };
+const DEFAULT_STYLE = { width: '100%', height: 500 };
 export default function Card(props) {
-  const currentProps = { ...DEFAULT_CONFIG, ...props };
+  const currentProps = {
+    ...DEFAULT_CONFIG,
+    ...props,
+    style: { ...DEFAULT_STYLE, ...props.style },
+  };
 
   return <AntdCard {...currentProps}></AntdCard>;
 }
